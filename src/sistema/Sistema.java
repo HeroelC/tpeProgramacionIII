d046ff30ff8db0d19c32e7016db55e815a7d6c39
Heroel <HeroelJCarpinetti@gmail.com>
2019-05-10 19:52:36 -0300

    Incorporada funcionalidad para el Servicio 2

--- src/sistema/Sistema.java
+++ src/sistema/Sistema.java
@@ -4,18 +4,19 @@ import java.util.ArrayList;
 
 public class Sistema {
 
-	//CONSTANTES
+	// CONSTANTES
 	private final int NO_VISITADO = 0;
 	private final int VISITADO = 1;
-	private final int TERMINADO = 2;
-	
+
 	// ATRIBUTOS
 	private ArrayList<Aeropuerto> aeropuertos;
 	private ArrayList<Reserva> reservas;
+	private ArrayList<ArrayList<Ruta>> vuelos; 
 
 	public Sistema() {
 		aeropuertos = new ArrayList<Aeropuerto>();
 		reservas = new ArrayList<Reserva>();
+		vuelos = new ArrayList<ArrayList<Ruta>>();
 	}
 
 	public void addRuta(Ruta ruta) {
@@ -66,8 +67,8 @@ public class Sistema {
 	}
 
 	/*
-	 * Añadir aeropuertos al sistema, se cambio a que reciba solamente el
-	 * aeropuerto y no todo los parametros
+	 * Añadir aeropuertos al sistema, se cambio a que reciba solamente el aeropuerto
+	 * y no todo los parametros
 	 */
 	public void addAeropuerto(Aeropuerto aeropuerto) {
 		aeropuertos.add(aeropuerto);
@@ -110,22 +111,23 @@ public class Sistema {
 		}
 		return aux;
 	}
-	//Funcion para listar las reservas [Item 2 del TP]
+
+	// Funcion para listar las reservas [Item 2 del TP]
 	public ArrayList<Reserva> getReservas() {
 		return new ArrayList<Reserva>(reservas);
 	}
-	
+
 	public void addReserva(Reserva reserva) {
-		
+
 		Aeropuerto aOrigen = devolverAeropuerto(reserva.getOrigen());
 		Aeropuerto aDestino = devolverAeropuerto(reserva.getDestino());
-		
-		reservas.add(reserva); //Agregamos la reserva al ArrayList de reservas del sistema
-		
+
+		reservas.add(reserva); // Agregamos la reserva al ArrayList de reservas del sistema
+
 		ArrayList<Ruta> aux = new ArrayList<Ruta>(getRutas());
-		for (int i = 0 ; i < aux.size(); i++) {
+		for (int i = 0; i < aux.size(); i++) {
 			if ((aux.get(i).getOrigen().equals(aOrigen) && (aux.get(i).getDestino().equals(aDestino)))) {
-				for (int j = 0 ; j < aux.get(i).getAerolineas().size(); j++) {
+				for (int j = 0; j < aux.get(i).getAerolineas().size(); j++) {
 					if (aux.get(i).getAerolineas().get(j).getNombre().equals(reserva.getAerolinea())) {
 						aux.get(i).getAerolineas().get(j).setReservas(reserva);
 					}
@@ -133,4 +135,44 @@ public class Sistema {
 			}
 		}
 	}
+
+	public ArrayList<ArrayList<Ruta>> obtenerVuelosDisponibles(int origen, int destino) {
+
+		vuelos = new ArrayList<ArrayList<Ruta>>();
+		ArrayList<Ruta> rutasOrigen = aeropuertos.get(origen - 1).getRutas();
+		ArrayList<Ruta> caminoActual = new ArrayList<>();
+
+		for (int i = 0; i < aeropuertos.size(); i++) {
+			aeropuertos.get(i).setEstado(NO_VISITADO);
+		}
+
+		for (int i = 0; i < rutasOrigen.size(); i++) {
+			if (rutasOrigen.get(i).getOrigen().getEstado() == NO_VISITADO) {
+				dfs_visit(rutasOrigen.get(i), aeropuertos.get(destino - 1), caminoActual);
+			}
+		}
+		
+		return vuelos;
+	}
+
+	private void dfs_visit(Ruta ruta, Aeropuerto destino, ArrayList<Ruta> caminoActual) {
+
+		ruta.getOrigen().setEstado(VISITADO);
+
+		if (ruta.getDestino().equals(destino)) {
+			caminoActual.add(ruta); //Agregamos a ruta que es el destino que buscamos.
+			vuelos.add(caminoActual); //Devolvemos el camino de como llegamos hasta ahi. 
+		} else {
+			ArrayList<Ruta> rutas = ruta.getDestino().getRutas();
+			for (int i = 0; i < rutas.size(); i++) {
+				if (rutas.get(i).getDestino().getEstado() == NO_VISITADO) {
+					caminoActual.add(rutas.get(i));
+					dfs_visit(rutas.get(i), destino, caminoActual);
+					caminoActual.remove(caminoActual.size() - 1);
+				}
+			}
+			ruta.getOrigen().setEstado(NO_VISITADO);
+		}
+	}
+
 }
